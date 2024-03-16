18e7666a604bbc5fd8f4db13ff9f2cf6e7ecdfec
XuQingLi <2542398121@qq.com>
2024-03-16 10:20:05 +0000

    modify_train

--- model.py
+++ model.py
@@ -1,30 +1,34 @@
 from torch import nn
 import torch
+
+
 # 搭建神经网络
 class Test(nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.model=nn.Sequential(
-        # Stride 表示在卷积核滑动时的步长或跨度
-        # Padding 是在输入数据周围添加额外的像素，以便更好地处理边缘信息。
-        # 这里的Padding=2是为了保持in/output输出的大小形状符合实际
-        nn.Conv2d(3,32,5,1,2),
-        nn.MaxPool2d(2),
-        nn.Conv2d(32,32,5,1,2),
-        nn.MaxPool2d(2),
-        nn.Conv2d(32,64,5,1,2),
-        nn.Flatten(),
-        nn.Linear(64*4*4,64),
-        nn.Linear(64,10) 
+        self.model = nn.Sequential(
+            # Stride 表示在卷积核滑动时的步长或跨度
+            # Padding 是在输入数据周围添加额外的像素，以便更好地处理边缘信息。
+            # 这里的Padding=2是为了保持in/output输出的大小形状符合实际
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.Flatten(),
+            nn.Linear(64 * 8 * 8, 64),
+            nn.Linear(64, 10),
         )
-    def forward(self,x):
-        x=self.model(x)
-        return x 
-if __name__=='__main__':
-    test=Test()
-    # 输入的尺寸
-    input=torch.ones((64,3,32,32))
-    output=test(input)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+if __name__ == "__main__":
+    test = Test()
+    # 输入的尺寸 torch.ones用于创建一个张量，其中所有元素的值都被设置为1。
+    # 包含了64个彩色图像，每个图像的大小为32x32像素，具有RGB三个通道，且所有像素的值都为1。
+    input = torch.ones((64, 3, 32, 32))
+    output = test(input)
     print(output.shape)
-    
-    
--- train.py
+++ train.py
@@ -2,46 +2,76 @@ import torchvision
 from torch import nn
 from model import *
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 # 准备训练集和测试集
-train_data=torchvision.datasets.CIFAR10(root="../data",train=True,transform=torchvision.transforms.ToTensor(),download=True)
-test_data=torchvision.datasets.CIFAR10(root="../data",train=False,transform=torchvision.transforms.ToTensor(),download=True)
+train_data = torchvision.datasets.CIFAR10(
+    root="../data",
+    train=True,
+    transform=torchvision.transforms.ToTensor(),
+    download=True,
+)
+test_data = torchvision.datasets.CIFAR10(
+    root="../data",
+    train=False,
+    transform=torchvision.transforms.ToTensor(),
+    download=True,
+)
 
-train_data_size=len(train_data)
-test_data_size=len(test_data)
+train_data_size = len(train_data)
+test_data_size = len(test_data)
 print("TrainDataSize:{}".format(train_data_size))
 print("TestDataSize:{}".format(train_data_size))
 
-# 利用dataloader来加载数据集
-train_dataloader=DataLoader(train_data,batch_size=64)
-test_dataloader=DataLoader(test_data,batch_size=64)
+# 利用dataloader来加载数据集   batch_size模型训练时每次更新参数的样本数量
+train_dataloader = DataLoader(train_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
 
 # 创建网络模型
-test=Test()
+test = Test()
 
 # 创建损失函数
-loss_fn=nn.CrossEntropyLoss()
+loss_fn = nn.CrossEntropyLoss()
 # 定义优化器,随机梯度下降
-# lr是learning_rate 
-optimizer=torch.optim.SGD(test.parameters(),lr=0.01,)
+# lr是learning_rate
+optimizer = torch.optim.SGD(
+    test.parameters(),
+    lr=0.01,
+)
 
 # 设置训练网络的一些参数
 # 记录训练的次数
-total_train_step=0
+total_train_step = 0
 # 记录测试的次数
-total_test_step=0
+total_test_step = 0
 # 训练的轮数
-epoch=10
+epoch = 10
+writer=SummaryWriter("./logs_model_train")
 for i in range(epoch):
-    print("第{}轮训练".format(i+1))
+    print("第{}轮训练".format(i + 1))
     # 训练步骤开始
     for data in train_dataloader:
-        imgs,targets=data
-        output=test(imgs)
-        loss=loss_fn(output,targets)
+        imgs, targets = data
+        output = test(imgs)
+        loss = loss_fn(output, targets)
         # 优化器调优
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        total_train_step=total_train_step+1
-        print("训练次数：{},loss:{}".format(total_train_step,loss))
-        
+        total_train_step = total_train_step + 1
+        if total_train_step%100==0:
+            print("训练次数：{},loss:{}".format(total_train_step, loss.item()))
+            writer.add_scalar("train_loss",loss.item(),total_train_step)
+    # 测试步骤开始
+    total_test_loss=0
+    with torch.no_grad():
+        for data in test_dataloader:
+            imgs,targets=data
+            outputs=test(imgs)
+            loss=loss_fn(outputs,targets)
+            total_test_loss=total_test_loss+loss.item()
+    print("total_test_loss:{}".format(total_test_step))
+    writer.add_scalar("test_loss",total_test_step,total_test_step)
+    total_test_step=total_test_step+1 
+    torch.save(test,"test_{}.pth".format(i))
+writer.close()                    
+            
